71e555065f9e1754f0eaf40081d4423449060a23
Daniel Ilievski <daniel_101699@hotmail.com>
2022-07-08 18:10:36 +0200

    Created a new Android project and setup a .gitignore file

---
+++ app/src/main/java/com/infinite_movies/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.infinite_movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shows_daniel_ilievski.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
