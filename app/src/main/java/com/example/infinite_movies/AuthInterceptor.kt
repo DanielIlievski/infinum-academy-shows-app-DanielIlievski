6397ad6ca9dffc8e6b90535a633fe3844d08cc2f
Daniel Ilievski <daniel_101699@hotmail.com>
2022-07-28 23:45:49 +0200

    Added AuthInterceptor

---
+++ app/src/main/java/com/example/infinite_movies/AuthInterceptor.kt
@@ -0,0 +1,28 @@
+package com.example.infinite_movies
+
+import android.content.Context
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor (context: Context): Interceptor {
+
+    private val sessionManager = SessionManager(context)
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+
+        sessionManager.fetchAuthToken()?.let { userToken ->
+            requestBuilder.addHeader(name = "access-token", value = userToken)
+        }
+
+        sessionManager.fetchClient()?.let { client ->
+            requestBuilder.addHeader("client", client)
+        }
+
+        sessionManager.fetchUid()?.let { uid ->
+            requestBuilder.addHeader("uid", uid)
+        }
+
+        return chain.proceed(requestBuilder.build())
+    }
+}
